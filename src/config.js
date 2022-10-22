8f2cefb2e9fc82a9392aa54f28bea43516d1c5f8
LKD70 <lkdornan70@gmail.com>
2022-10-22 01:39:05 +0100

    Add nodemon to dev dependencies Add internal config file

---
+++ src/config.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import user_config from "../config.js";
+
+const config = {
+	telegram: {
+		token: process.env?.TELEGRAM_TOKEN || user_config?.telegram?.token,
+	},
+	openWeatherMap: {
+		token: process.env?.OPENWEATHERMAP_TOKEN || user_config?.openWeatherMap?.token,
+	}
+};
+
+if (config.telegram.token === undefined) {
+	throw new Error("Telegram token is not defined");
+}
+
+export default config;
